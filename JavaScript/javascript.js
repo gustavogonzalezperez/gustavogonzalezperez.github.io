30e86768f768b2b358bcaa57f2d8b66fd1b13809
gustavogonzalezperez <153717599+gustavogonzalezperez@users.noreply.github.com>
2023-12-13 01:41:43 -0700

    update

--- JavaScript/javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#menu').click(function() {
-        $('#mobile-nav').fadeIn();
-    })
-    $('#close-menu').click(function(){
-        $('#mobile-nav').fadeOut();
-    })
-});
